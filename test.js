18cca2ed387cac9a303ec81e826a826bc0451f6e
Ahn sa <se_a530@naver.com>
2021-05-07 15:14:13 +0900

    Test1

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log('hello react!!!');
+function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    // highlight-next-line
+    ReactDOM.render(element, document.getElementById('root'));
+  }
+  
+  setInterval(tick, 1000);
