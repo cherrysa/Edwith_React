e220a9ea961882f59f75793b93947842021b35d9
Ahn sa <se_a530@naver.com>
2021-05-17 16:37:51 +0900

    Project : 네이버 부스트 코스 예약시스템 : 리액트 라이브러리 적용하기  -------------------------------------------------------------------------------- Problem : main.html 리액트로 구현하기  Analysis :  예약시스템의 ui는 이미 만들어져 있는 상태이다(html, css)가 존재한다. -> 따라서 리액트 공식문서를 참고하여 개발프로세스를 적용한다.  Phostshop으로 만들어진 ui라고 했는데 어차피 html/css도 같은 맥락으로 간주한다.   정확히 얘기하면 슈도코드라기 보단 개발 프로세스를 따라하는것으로 템플릿을 정의하는게  맞다고 생각한다. 라이브러리를 적용하는것이기에 기능이나 변동사항이 없기 때문이다.  Proccess 1. UI를 컴포넌트 계층 구조로 나누기 (main.html 계층구조로 나누기) 2. React를 적용한 정적인 버전 만들기   Result : UI 컴포넌트 루트에 전체 컴포넌트에 띄우기

--- naver-react-app/src/App.js
+++ naver-react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Never Give up Seah!
         </a>
       </header>
     </div>
